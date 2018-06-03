4d628e0ce42c39c209d66348cfdd4242475566c9
locnguyen <vanloctech@gmail.com>
2018-06-04 00:24:43 +0700

    v1.2

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var chatRouter = require('./routes/chat');
+var tictoeRouter = require('./routes/tictoe');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/chat', chatRouter);
+app.use('/tictoe', tictoeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- socket.js
+++ socket.js
@@ -2,11 +2,19 @@ var socket_server_client = (server) => {
     //use socket.io
     var io = require("socket.io")(server);
     //event connection
+    var list = [];
+    var i = 0;
     io.on("connection", function (client) {
         console.log("Co nguoi dang ket noi: " + client.id);
-
+        list[i++] = client.id;
         client.on('chat message', function(msg){
-            io.emit('res message', client.id + ": " + msg);
+            // console.log(msg.msgtex);
+            // console.log(msg.coun);
+            // console.log(msg.ele);
+            // console.log(client.id);
+            msg.coun += 1;
+            io.emit('res message',msg);
+            // console.log(list);
         });
 
         client.on("disconnect", function () {
